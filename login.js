0678edcd17129b616743ce3c8b3a5d392cebd1a7
myriambyu <myriama@byu.edu>
2024-02-27 10:26:24 -0700

    messed around with login

--- login.js
+++ login.js
@@ -3,3 +3,14 @@ function login() {
     localStorage.setItem("userName", playerName.value);
     window.location.href = "practice.html";
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const username = document.getElementById("username").value;
+        localStorage.setItem("username", username);
+        document.getElementById("userInfo").style.display = "block";
+        document.getElementById("loggedInUsername").textContent = username;
+    });
+  
+  });
--- practice.js
+++ practice.js
@@ -1,6 +1,8 @@
-const playerName = document.querySelector('.user-name');
+function DisplayUserName () {
+    const playerName = document.querySelector('.User-name');
     playerName.textContent = this.getPlayerName();
 
     getPlayerName() {
         return localStorage.getItem('userName') ?? 'Unknown';
-      }
+    }   
+}
