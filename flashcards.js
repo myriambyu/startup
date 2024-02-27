6b1fdcdb54831ac52d65ce957da3d77e7f806f32
myriambyu <myriama@byu.edu>
2024-02-27 14:34:24 -0700

    added constants

--- flashcards.js
+++ flashcards.js
@@ -2,3 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const storedUsername = localStorage.getItem("username");
     document.getElementById("loggedInUsername").textContent = storedUsername; 
   });
+
+const card = document.querySelector("#vocab");
+const front = document.querySelector(".front");
+const back = document.querySelector(".back");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const questions = [];
+
+let current = 0;
+let turned = false;
