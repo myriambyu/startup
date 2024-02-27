892153fb8ebb446fa5c2811339f40e7320b2bbc6
myriambyu <myriama@byu.edu>
2024-02-27 14:44:11 -0700

    updated flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -8,7 +8,23 @@ const front = document.querySelector(".front");
 const back = document.querySelector(".back");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
-const questions = [];
+const questions = [
+  { question: "What is the capital of France?", answer: "Paris" },
+  { question: "What is 2 + 2?", answer: "4" },
+  { question: "Who wrote 'Romeo and Juliet'?", answer: "William Shakespeare" }];
 
 let current = 0;
 let turned = false;
+
+function resetCard() {
+  prev.disabled = false;
+  next.disabled = false;
+  card.classList.remove("turned");
+}
+
+function populateNextCard() {
+  resetCard();
+  front.innerHTML = `<p>${questions[current].question}</p>`;
+  back.innerHTML = `<p>${questions[current].answer}</p>`;
+  current++;
+}
