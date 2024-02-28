34c4d65c533903b6ddc377380411401d7c1e0a0c
myriambyu <myriama@byu.edu>
2024-02-27 17:54:40 -0700

    added web socket space

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("loginForm").addEventListener("submit", function(event) {
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function login(event) {
       event.preventDefault();
       var username = document.getElementById("username").value;
       localStorage.setItem("username", username);
       window.location.href = "practice.html";
     });
   });
-  
--- progress.js
+++ progress.js
@@ -12,4 +12,36 @@ progressBars.forEach(progressBar => {
     progressBar.value = storedProgress * 20;
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const percentElement = document.querySelector('.percent');
 
+  percentElement.textContent = 20 * storedProgress + "%";
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const percentElement = document.querySelector('.foodPercent');
+
+  percentElement.textContent = 20 * storedProgress + "%";
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const percentElement = document.querySelector('.colorsPercent');
+
+  percentElement.textContent = 20 * storedProgress + "%";
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const percentElement = document.querySelector('.verbsPercent');
+
+  percentElement.textContent = 20 * storedProgress + "%";
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const countInput = document.getElementById('count');
+  
+  function increaseCount() {
+      
+      let currentValue = parseInt(countInput.value);
+      currentValue++;
+      countInput.value = currentValue;
+  }
+  setInterval(increaseCount, 5000);
+});
