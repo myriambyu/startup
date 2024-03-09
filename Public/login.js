a48f9497bb1cbc3ab0a6eeed88b6af2e52ea3ae9
myriambyu <myriama@byu.edu>
2024-03-09 10:29:15 -0700

    added index.js

--- Public/login.js
+++ Public/login.js
@@ -2,7 +2,7 @@
   document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("loginForm").addEventListener("submit", function login(event) {
       event.preventDefault();
-      var username = document.getElementById("username").value;
+      const username = document.getElementById("username").value;
       localStorage.setItem("username", username);
       window.location.href = "practice.html";
     });
