3cf4459f5c6c728cdb617f47f582aae033ea1f76
myriambyu <myriama@byu.edu>
2024-03-11 13:11:14 -0600

    updated progress bars

--- Public/progress.js
+++ Public/progress.js
@@ -1,49 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const storedUsername = localStorage.getItem("username");
-    document.getElementById("loggedInUsername").textContent = storedUsername; 
-});
+document.addEventListener("DOMContentLoaded", async function() {
+  const storedUsername = localStorage.getItem("username");
+  document.getElementById("loggedInUsername").textContent = storedUsername; 
 
-
-document.addEventListener("DOMContentLoaded", async function loadProgress() {
   let storedProgress = null;
   try {
-    // Get the storedProgress from the service
+
     const response = await fetch('/api/storedProgress');
     storedProgress = await response.json();
     localStorage.setItem("storedProgress", storedProgress);
   } catch (error) {
-    // If there was an error, log it and use the last saved progress
+  
     console.error('Error fetching storedProgress:', error);
     storedProgress = localStorage.getItem("storedProgress");
   }
-});
 
-const progressBars = document.querySelectorAll('.progress-bar');
-
-progressBars.forEach(progressBar => {
+  const progressBars = document.querySelectorAll('.progress-bar');
+  progressBars.forEach(progressBar => {
     progressBar.value = storedProgress * 20;
-});
+  });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const percentElement = document.querySelector('.percent');
-
-  percentElement.textContent = 20 * storedProgress + "%";
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const percentElement = document.querySelector('.foodPercent');
-
-  percentElement.textContent = 20 * storedProgress + "%";
-});
-document.addEventListener('DOMContentLoaded', function() {
-  const percentElement = document.querySelector('.colorsPercent');
-
-  percentElement.textContent = 20 * storedProgress + "%";
-});
-document.addEventListener('DOMContentLoaded', function() {
-  const percentElement = document.querySelector('.verbsPercent');
-
-  percentElement.textContent = 20 * storedProgress + "%";
+  const percentElements = document.querySelectorAll('.percent');
+  percentElements.forEach(percentElement => {
+    percentElement.textContent = (storedProgress * 20) + "%";
+  });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
