4792406613c8349db31f5203977e34b47d034840
myriambyu <myriama@byu.edu>
2024-03-21 15:27:07 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
+
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
