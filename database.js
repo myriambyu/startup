194d4bba74ca264e716fe411c82103417101b2f4
myriambyu <myriama@byu.edu>
2024-03-21 15:13:45 -0600

    updated database.js

--- database.js
+++ database.js
@@ -18,3 +18,24 @@ const scoreCollection = db.collection('progress');
   process.exit(1);
 });
 
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
