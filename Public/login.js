ecf68a1a012207cb6454010e593d13c3df1d0689
myriambyu <myriama@byu.edu>
2024-03-21 16:58:21 -0600

    updated login.js

--- Public/login.js
+++ Public/login.js
@@ -17,7 +17,36 @@ function setDisplay(controlId, display) {
     playControlEl.style.display = display;
   }
 }
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
 
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('username', username);
+    window.location.href = 'practice.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
 
 
   document.addEventListener("DOMContentLoaded", function() {
