f0d3afd62a0308169fe4aa6e60652d789a6c9b0b
myriambyu <myriama@byu.edu>
2024-03-21 16:56:17 -0600

    updated login.js

--- Public/login.js
+++ Public/login.js
@@ -1,3 +1,24 @@
+(async () => {
+  const username = localStorage.getItem('username');
+  if (username) {
+    document.querySelector('#username').textContent = username;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
+
+
 
   document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("loginForm").addEventListener("submit", function login(event) {
